db98f33aa074116a0e921029c5b3cd402228cbc2
Ellis <ellisgraham97@hotmail.co.uk>
2022-10-24 12:11:49 +0100

    JPA ID's
    Added required data to ensure that the JPA's are official entities

--- src/main/java/guru/springframework/spring5webapp/domain/Author.java
+++ src/main/java/guru/springframework/spring5webapp/domain/Author.java
@@ -1,9 +1,17 @@
 package guru.springframework.spring5webapp.domain;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
 import java.util.Set;
 
+@Entity
 public class Author {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id; //primaryKey
     private String firstName;
     private String lastName;
     private Set<Book> books;
@@ -16,6 +24,14 @@ public class Author {
         this.firstName = firstName;
         this.lastName = lastName;
         this.books = books;
+    }
+
+    public Long getId(){
+        return id;
+    }
+
+    public void setId(){
+        this.id = id;
     }
 
     public String getFirstName() {
